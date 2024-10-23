ee02bd57b8b60369f230437ba537dace88e9f58d
Ilya Cherney <ilyacherney@yandex.ru>
2024-10-23 09:48:04 +0300

    Добавить обработку запросов в цикле и тред-пуле

--- src/main/java/ru/otus/october/http/server/HttpServer.java
+++ src/main/java/ru/otus/october/http/server/HttpServer.java
@@ -3,6 +3,8 @@ package ru.otus.october.http.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     private int port;
@@ -15,17 +17,18 @@ public class HttpServer {
 
     public void start() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Сервер запущен на порту: " + port);
-            try (Socket socket = serverSocket.accept()) {
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                String rawRequest = new String(buffer, 0, n);
-                HttpRequest request = new HttpRequest(rawRequest);
-                request.info(true);
-                dispatcher.execute(request, socket.getOutputStream());
+            while (true) {
+                System.out.println("Сервер запущен на порту: " + port);
+                Socket socket = serverSocket.accept();
+                ExecutorService executorService = Executors.newFixedThreadPool(3);
+                executorService.execute(new ClientHandler(this, socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public Dispatcher getDispatcher() {
+        return dispatcher;
     }
 }
